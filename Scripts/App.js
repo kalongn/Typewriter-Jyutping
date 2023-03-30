c099d4e4e4d837ddca1ba646bd0217a67734405b
Ka Long Ngai <kls8162@gmail.com>
2023-03-30 15:00:03 -0400

    Updated: animation of theme icon changes.

---
+++ Scripts/App.js
@@ -0,0 +1,31 @@
+let webpageTheme;
+
+function startWeb() {
+    webpageTheme = true;
+}
+
+function switchThemes() {
+    let currentTheme = document.getElementById("themes");
+    if (webpageTheme) {
+        currentTheme.classList.toggle("swap");
+        setTimeout(function () {
+            currentTheme.src = "../Img/moon_icon.png";
+            currentTheme.alt = "Dark Mode";
+        }, 150);
+        setTimeout(function () {
+            currentTheme.classList.remove("swap");
+        }, 310);
+
+    } else {
+        currentTheme.classList.toggle("swap");
+        setTimeout(function () {
+            currentTheme.src = "../Img/sun_icon.png";
+            currentTheme.alt = "Light Mode";
+        }, 150);
+        setTimeout(function () {
+            currentTheme.classList.remove("swap");
+        }, 310);
+    }
+    webpageTheme = !webpageTheme;
+    return false;
+}
