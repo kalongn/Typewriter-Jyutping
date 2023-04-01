58ea6d2848e119037931c7a024ec42431a8faab5
Ka Long Ngai <kls8162@gmail.com>
2023-04-01 13:40:11 -0400

    Modified: optimized switch themes method.

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,31 +1,27 @@
 let webpageTheme;
+const currentTheme = document.getElementById("themes");
 
 function startWeb() {
     webpageTheme = true;
 }
 
 function switchThemes() {
-    let currentTheme = document.getElementById("themes");
+    currentTheme.classList.toggle("swap");
     if (webpageTheme) {
-        currentTheme.classList.toggle("swap");
         setTimeout(function () {
             currentTheme.src = "../Img/moon_icon.png";
             currentTheme.alt = "Dark Mode";
         }, 150);
-        setTimeout(function () {
-            currentTheme.classList.remove("swap");
-        }, 300);
 
     } else {
-        currentTheme.classList.toggle("swap");
         setTimeout(function () {
             currentTheme.src = "../Img/sun_icon.png";
             currentTheme.alt = "Light Mode";
         }, 150);
-        setTimeout(function () {
-            currentTheme.classList.remove("swap");
-        }, 300);
     }
+    setTimeout(function () {
+        currentTheme.classList.remove("swap");
+    }, 300);
     webpageTheme = !webpageTheme;
     return false;
 }
