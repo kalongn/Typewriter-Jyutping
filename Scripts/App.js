d7e642dc409073a2de3bf3cc4aed12ea6d3f3942
Ka Long Ngai <kls8162@gmail.com>
2023-04-10 11:04:54 -0400

    Updated: restartButton works.

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,3 +1,11 @@
+window.addEventListener("load", function () {
+    let loader = document.getElementById("loading-screen-wrapper");
+    let mainpage = document.getElementById("main-page-wrapper");
+    console.log(loader.classList);
+    loader.classList.toggle("fade");
+    setTimeout(function () { loader.remove(); mainpage.style.display = "grid"; }, 800);
+})
+
 let webpageTheme;
 const currentTheme = document.getElementById("themes");
 
@@ -25,3 +33,28 @@ function switchThemes() {
     webpageTheme = !webpageTheme;
     return false;
 }
+
+const restartButton = document.getElementById("restart-test-button");
+const hotkey = ['ShiftLeft', 'KeyR'];
+const keysPressed = new Set();
+
+function restartButtonFunction() {
+    console.log("It works");
+}
+
+document.addEventListener('keydown', event => {
+    keysPressed.add(event.code);
+    if (event.code === 'ShiftLeft') {
+        restartButton.classList.add('selected');
+    }
+    for (const key of hotkey) {
+        if (!keysPressed.has(key)) {
+            return;
+        }
+    }
+    restartButton.click();
+    restartButton.classList.remove('selected');
+});
+document.addEventListener('keyup', event => {
+    keysPressed.delete(event.code);
+});
